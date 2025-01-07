329133caa0e9cdfbe2b8c6d3987e9dd710abc348
betulgurturk <betul__1997@hotmail.com>
2025-01-07 22:11:43 +0300

    validation yeri değiştirildi

--- Application/Behaviors/Validation/ValidationBehavior.cs
+++ Application/Behaviors/Validation/ValidationBehavior.cs
@@ -1,7 +1,7 @@
 using FluentValidation;
 using MediatR;
 
-namespace QuickPost.Application.Behaviors;
+namespace QuickPost.Application.Behaviors.Validation;
 
 public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using QuickPost.Application.Behaviors;
+using QuickPost.Application.Behaviors.Validation;
 using System.Reflection;
 using FluentValidation;
 
